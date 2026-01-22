cae345792cd2d32adadbba06e72a28eb5b9819bb
damouu <mouadsehbaoui@gmail.com>
2026-01-22 13:41:46 +0100

    Refactor: index.js (#LAC-1)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
+import commentRouter from "./routes/comment.routes.js";
+
+import {register, trackRequests} from "./utils/metrics.js";
 
 const app = express();
-const uri = process.env.MONGO_URI; // from docker-compose
+const uri = process.env.MONGO_URI;
 
 mongoose.connect(uri)
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error("MongoDB error:", err));
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.use('/api/comment', commentRouter);
+app.use(trackRequests);
 
-app.get("/api/status", (req, res) => {
-    res.json({status: "OK", time: new Date()});
+app.get('/metrics', async (req, res) => {
+    res.setHeader('Content-Type', register.contentType);
+    res.send(await register.metrics());
 });
+
+app.listen(3000, () => console.log("Server running on port 3000"));
