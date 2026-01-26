102d996b4517e31290c45049f013e3fd110447f3
damouu <mouadsehbaoui@gmail.com>
2026-01-26 22:33:33 +0100

    Refactor: index.js (#LAC-2)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
 import commentRouter from "./routes/comment.routes.js";
-
+import dotenv from 'dotenv';
 import {register, trackRequests} from "./utils/metrics.js";
+import app from './app.js';
 
-const app = express();
+dotenv.config();
+
 const uri = process.env.MONGO_URI;
 
 mongoose.connect(uri)
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error("MongoDB error:", err));
 
-app.use('/api/comment', commentRouter);
+app.use(express.json());
 app.use(trackRequests);
+app.use('/api/', commentRouter);
 
 app.get('/metrics', async (req, res) => {
     res.setHeader('Content-Type', register.contentType);
     res.send(await register.metrics());
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
