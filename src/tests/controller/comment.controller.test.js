bf7687e060325f5f48151378b53060c13a3f94ec
damouu <mouadsehbaoui@gmail.com>
2026-01-27 17:53:12 +0100

    Test: comment.controller.test.js  (#LAC-4)

--- src/tests/controller/comment.controller.test.js
+++ src/tests/controller/comment.controller.test.js
@@ -1,7 +1,7 @@
 import {jest} from '@jest/globals';
 import request from 'supertest';
 
-const VALID_UUID = 'c26b8105-3686-4415-ae16-bf70c58f1b3d';
+const VALID_UUID = '68e35d01-509f-4378-bf62-f4a8c8d58acb';
 
 await jest.unstable_mockModule('../../middleware/auth.middleware.js', () => ({
     authMiddleware: (req, res, next) => {
@@ -17,16 +17,17 @@ await jest.unstable_mockModule('../../middleware/auth.middleware.js', () => ({
 
 
 await jest.unstable_mockModule('../../services/comment.service.js', () => ({
-    saveComment: jest.fn()
+    saveComment: jest.fn(), updateComment: jest.fn(),
 }));
 
-const {saveComment} = await import('../../services/comment.service.js');
+
+const commentService = await import('../../services/comment.service.js');
 const {default: app} = await import('../../app.js');
 
 describe('POST /api/chapter/:chapterUuid/comment', () => {
 
     it('creates a comment', async () => {
-        saveComment.mockResolvedValueOnce({
+        commentService.saveComment.mockResolvedValueOnce({
             _id: 'mock-id', comment: 'Hello world', userName: 'Test User', chapterUUID: VALID_UUID
         });
 
@@ -35,15 +36,43 @@ describe('POST /api/chapter/:chapterUuid/comment', () => {
             .send({comment: 'Hello world'});
 
         expect(res.statusCode).toBe(201);
-        expect(saveComment).toHaveBeenCalledTimes(1);
+        expect(commentService.saveComment).toHaveBeenCalledTimes(1);
     });
 
 
     it('fails when service throws', async () => {
-        saveComment.mockRejectedValueOnce(new Error('Database down'));
+        commentService.saveComment.mockRejectedValueOnce(new Error('Database down'));
 
         const res = await request(app)
             .post(`/api/chapter/${VALID_UUID}/comment`)
+            .send({comment: 'Hello world'});
+
+        expect(res.statusCode).toBe(500);
+    });
+
+});
+
+
+describe('PUT /api/comment/:commentUuid', () => {
+    it('updates a comment', async () => {
+        commentService.updateComment.mockResolvedValueOnce({
+            comment: 'update', commentUuid: VALID_UUID
+        });
+
+        const res = await request(app)
+            .put(`/api/comment/${VALID_UUID}`)
+            .send({comment: 'update!', commentUuid: VALID_UUID});
+
+        expect(res.statusCode).toBe(204);
+        expect(commentService.updateComment).toHaveBeenCalledTimes(1);
+    });
+
+
+    it('fails when service throws', async () => {
+        commentService.updateComment.mockRejectedValueOnce(new Error('Database down'));
+
+        const res = await request(app)
+            .put(`/api/comment/${VALID_UUID}`)
             .send({comment: 'Hello world'});
 
         expect(res.statusCode).toBe(500);
