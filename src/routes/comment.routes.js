35ccf5c58626b5eb0b027b434cad3669733721ce
damouu <mouadsehbaoui@gmail.com>
2026-01-27 17:52:54 +0100

    Refactor: comment.routes.js (#LAC-4)

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -2,15 +2,10 @@ import {Router} from "express";
 import {authMiddleware} from '../middleware/auth.middleware.js';
 import * as commentController from '../controllers/comment.controller.js';
 
-
 const router = Router();
 
-const saveComment = async (req, res) => {
-    const comment = await commentController.createComment(req, res);
-    return res.json({status: comment.status, comment: comment});
-}
+router.post('/chapter/:chapterUuid/comment', authMiddleware, commentController.createComment);
 
-router.post('/chapter/:chapterUuid/comment', authMiddleware, saveComment);
-
+router.put('/comment/:commentUuid', authMiddleware, commentController.updateComment);
 
 export default router;
