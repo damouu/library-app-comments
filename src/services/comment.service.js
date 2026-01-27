89b3815f4086fe47f5959667da92458f5c3464bf
damouu <mouadsehbaoui@gmail.com>
2026-01-27 17:52:05 +0100

    Feature: comment.service.js (#LAC-4)

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -4,3 +4,7 @@ import * as commentRepository from '../repository/comment.repository.js';
 export const saveComment = async ({comment, chapterUUID, memberCardUuid, userName, userEmail, avatar_URL}) => {
     return await commentRepository.create(comment, chapterUUID, memberCardUuid, userName, userEmail, avatar_URL);
 }
+
+export const updateComment = async ({comment, commentUuid}) => {
+    return await commentRepository.update(comment, commentUuid);
+}
