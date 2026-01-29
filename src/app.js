07243938a30d7ad02d96b21564fa12da15119488
damouu <mouadsehbaoui@gmail.com>
2026-01-29 21:33:45 +0100

    FIX: app.js (#LAC-9)

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ import express from 'express';
 import commentRouter from './routes/comment.routes.js';
 import {trackRequests} from './utils/metrics.js';
 
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const app = express();
 
 app.use(express.json());
 app.use(trackRequests);
+app.use(cors(corsOptions));
 app.use('/api', commentRouter);
 
 export default app;
