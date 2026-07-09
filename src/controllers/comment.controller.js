6886e6ebbcf2cee700da8d66b01fb70b4607d3e1
damouu <mouadsehbaoui@gmail.com>
2026-07-09 11:50:01 +0900

    Refactor: comment.controller.js (#LAC-13)

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,6 +1,7 @@
 import * as commentService from "../services/comment.service.js";
 import {sanitizeComment} from "../utils/sanitize.js";
 import {mapCreateCommentRequest} from "../dto/mapCreateCommentRequest.js";
+import {UpdateCommentRequestDTO} from "../dto/UpdateCommentRequestDTO.js";
 
 export const createComment = async (req, res) => {
     try {
@@ -18,16 +19,15 @@ export const createComment = async (req, res) => {
 
 
 export const updateComment = async (req, res) => {
-    try {
-        const {comment} = req.body;
-        const commentUuid = req.params.commentUuid;
-        const cleanComment = sanitizeComment(comment);
 
-        await commentService.updateComment({
-            comment: cleanComment, commentUuid: commentUuid, user_memberCardUUID: req.user.user_memberCardUUID
-        });
+    try {
+
+        const dto = new UpdateCommentRequestDTO(req.params.commentUuid, req.user.member_card_uuid, sanitizeComment(req.body.comment));
+
+        await commentService.updateComment(dto);
 
         res.status(204).send();
+
     } catch (error) {
         if (error.message.includes("Unauthorized")) {
             return res.status(403).json({message: error.message});
@@ -47,7 +47,7 @@ export const deleteComment = async (req, res) => {
         const commentUuid = req.params.commentUuid;
 
         await commentService.deleteComment({
-            commentUuid: commentUuid, user_memberCardUUID: req.user.user_memberCardUUID,
+            commentUuid: commentUuid, user_memberCardUUID: req.user.member_card_uuid,
         });
 
         res.status(204).send();
